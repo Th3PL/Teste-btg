006c4572d89dd70996e80806d2cbf589d6752127
Pedro Lucas <plucas2013@gmail.com>
2025-06-18 19:44:53 -0300

    Testando classe programa

--- Program.cs
+++ Program.cs
@@ -1,8 +1,37 @@
-﻿Aluno aluno = new Aluno();
-aluno.Nome = "Pedro";
-aluno.Nota1 = 9.0;
-aluno.Nota2 = 7.0;
-aluno.Nota3 = 6.0;
-Console.WriteLine(aluno.media());
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Aluno> alunos = new List<Aluno>();
 
+        while (true)
+        {
+            Console.Write("Digite o Nome do Aluno ou Enter para sair");
+            string nome = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                break;
+            }
+
+            Console.Write("Digite a Primeira Nota: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a Segunda Nota: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a Terceira Nota: ");
+            double n3 = Convert.ToDouble(Console.ReadLine());
+
+            Aluno aluno = new Aluno
+            {
+                Nome = nome,
+                Nota1 = n1,
+                Nota2 = n2,
+                Nota3 = n3
+            };
+            alunos.Add(aluno);
+        }
+    }
+}
 
--- obj/Debug/net8.0/Teste-btg.AssemblyInfo.cs
+++ obj/Debug/net8.0/Teste-btg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teste-btg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61c08ceba22d3253674a2491726c846c84247a17")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teste-btg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teste-btg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
